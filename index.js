75eadc2d9c8ea76ebdf721c779773da225a6114e
Damion McCurdy <damion.mccurdy@gmail.com>
2020-06-10 21:00:54 -0600

    Commit

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-//window.onload=function(){
-    document.addEventListener('DOMContentLoaded', function(){
-        // we code here
-      axios.get("https://www.marines.mil/").then(function(result){
-        console.log(result.data)
- 
-        })
+// wait till HTML loads then run this { } JavaScript
+document.addEventListener('DOMContentLoaded', function () {
+  let button = document.querySelector('#search-button');
+  let input = document.querySelector('#search-input');
+  let pokePic = document.querySelector('#pokePic');
+  let pokePic2 = document.querySelector('#pokePic2');
+  button.addEventListener('click', function () {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/' + input.value)
+      .then(function (result) {
+        console.log(result.data.sprites.front_default);
+        pokePic.src = result.data.sprites.front_default;
+        pokePic2.src = result.data.sprites.back_default;
       })
+      .catch(function(error){
+        console.log(error)
+        pokePic.src = 'https://vivianpetersoops.com/wp-content/uploads/2017/11/oops-orange.jpg'
+        pokePic2.src = ''
+      })
+  });
+});
